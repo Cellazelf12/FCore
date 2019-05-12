117f90e299a448d0c51c1c22e06b835fce4825fb
Tyler <Tyler@DESKTOP>
2019-05-12 03:37:57 -0500

    Removed .getFormat() piece of code from any code to do with formatting. Simplified everything a bit ;]

--- src/main/java/proj/format/core/format/Formats.java
+++ src/main/java/proj/format/core/format/Formats.java
@@ -3,7 +3,6 @@ package proj.format.core.format;
 import java.util.ArrayList;
 import java.util.List;
 
-import lombok.Getter;
 import proj.format.core.format.formats.Cape;
 import proj.format.core.format.formats.CrossFire;
 import proj.format.core.format.formats.Exact;
@@ -17,6 +16,7 @@ import proj.format.core.format.formats.Wing;
 import proj.format.core.format.formats.XYCircle;
 import proj.format.core.format.formats.XZCircle;
 import proj.format.core.format.formats.YZCircle;
+import proj.format.core.utilities.objects.Loc;
 
 public enum Formats {
     
@@ -26,10 +26,8 @@ public enum Formats {
     RANDOM_ROUND_LAYER(new RandomRoundLayer()), 
     EXACT(new Exact()),
     SQUARE(new Square()),
-//    PENNY(new Penny()),
     CROSS_FIRE(new CrossFire()),
     HELIX(new Helix()),
-//    FIREFLY(new Firefly()),
     WING(new Wing()),
     HAT(new Hat()),
     CAPE(new Cape()),
@@ -38,48 +36,104 @@ public enum Formats {
     YZ_CIRCLE(new YZCircle()),
     ;
     
-    @Getter
     private IFormat format;
     
     Formats(IFormat format) {
         this.format = format;
     }
     
+    /**
+     * 
+     * @return List of all formats
+     */
     public static List<String> getFormats() {
         Formats[] formats = Formats.values();
         List<String> list = new ArrayList<String>();
         for(Formats ifv : formats) {
-            list.add(ifv.getFormat().getString().toUpperCase());
+            list.add(ifv.getString().toUpperCase());
         }
         return list;
     }
     
-    public static IFormat getFormat(String format){
+    /**
+     * 
+     * @param format Format name
+     * @return IFormat value of format
+     */
+    public static Formats getFormat(String format){
         format = format.toUpperCase();
-        for(Formats iformat : Formats.values()){
-            if(iformat.getFormat().getString().toUpperCase().equals(format)){
-                return iformat.getFormat();
+        for(Formats formats : Formats.values()){
+            if(formats.getString().toUpperCase().equals(format)){
+                return formats;
             }
         }
-        return NONE.getFormat();
+        return NONE;
     }
     
+    /**
+     * 
+     * @param format Format name
+     * @return If format exists
+     */
     public static boolean exists(String format){
-        for(Formats iformats: Formats.values()){
-            if(iformats.getFormat().getString().equalsIgnoreCase(format)){
+        for(Formats formats: Formats.values()){
+            if(formats.getString().equalsIgnoreCase(format)){
                 return true;
             }
         }
         return false;
     }
+
     
+    /**
+     * 
+     * @return Name of the format
+     */
+    public String getString() {
+        return format.getString();
+    }
+
+    
+    /**
+     * 
+     * @return Name of the format
+     */
     public String getName() {
         return format.getString();
     }
+
     
+    /**
+     * 
+     * @return Name of the format
+     */
     @Override
     public String toString() {
         return format.getString();
+    }
+    
+    /**
+     * 
+     * @param location Location center
+     * @param mod Integer modifier used to slightly adjust the positioning of the format
+     * @return Locations of a single "frame" of the format
+     */
+    public List<Loc> getLocs(Loc location, Integer frame){
+        return format.getLocs(location, frame);
+    }
+    
+    /**
+     * 
+     * @param location Location center
+     * @param rate Used with 'loopable' formats to define the "definition" of the format
+     * @param many Used to specify return amount of duplicated locations, or amount of organized locations
+     * @param frame Gets the "Frame" of the format (0-[rate-1])
+     * @param width Width or Length of the format (modifies both X and Z coordinates)
+     * @param higth Height of the format (will not change Y center location)
+     * @return List of locations for specified frame of the format with other specified details
+     */
+    public List<Loc> getLocs(Loc location, Integer rate, Integer many, Integer frame, Double width, Double higth) {
+        return format.getLocs(location, rate, many, frame, width, higth);
     }
 
 }
